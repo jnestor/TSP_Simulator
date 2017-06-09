b055fa662290edc29290cb4b941ad48b4cd21d30
jnestor <nestorj@lafayette.edu>
2017-06-09 14:29:16 -0400

    adding a few basic files

---
+++ TSP_Simulator/src/tsp_simulator/VPE.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tsp_simulator;
+
+/**
+ * Virtual Processing Element
+ * @author nestorj
+ */
+public class VPE {
+    public boolean update() {
+        return false;
+    }
+    
+    private int x_pos;
+    private int y_pos;
+}
---
+++ TSP_Simulator/src/tsp_simulator/VPEArray.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tsp_simulator;
+
+/**
+ * VPEArray - Simulates full 2D array of VPEs
+ * essentiall a large 2D array
+ * @author nestorj
+ */
+public class VPEArray {
+    
+    public VPE getVPE(int x_pos, int y_pos) {
+        return null;
+    }
+}
